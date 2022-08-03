b680eb8a004c300037be3b6f5624f7e668b0077b
Thomas Fouquert <tfouquert@gmail.com>
2022-08-03 13:08:36 +0200

    vich_up_new

--- src/Entity/Food.php
+++ src/Entity/Food.php
@@ -5,8 +5,11 @@ namespace App\Entity;
 use App\Repository\FoodRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: FoodRepository::class)]
+#[Vich\Uploadable]
 class Food
 {
     #[ORM\Id]
@@ -25,6 +28,9 @@ class Food
     #[ORM\Column(length: 255)]
     private ?string $imageName = null;
 
+    #[Vich\UploadableField(mapping: 'food', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
     #[ORM\Column]
     private ?int $calorie = null;
 
@@ -40,6 +46,22 @@ class Food
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        // if (null !== $imageFile) {
+        //     // It is required that at least one field changes if you are using doctrine
+        //     // otherwise the event listeners won't be called and the file is lost
+        //     $this->updatedAt = new \DateTimeImmutable();
+        // }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getName(): ?string
